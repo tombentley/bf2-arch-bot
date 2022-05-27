a255c4f71999d56b35c48062b01209bdb9e9be1b
Tom Bentley <tbentley@redhat.com>
2022-05-27 13:30:09 +0100

    Add failing test, see quarkiverse/quarkus-github-app#307

--- src/test/java/org/bf2/arch/bot/CreateCommentTest.java
+++ src/test/java/org/bf2/arch/bot/CreateCommentTest.java
@@ -16,5 +16,37 @@
  */
 package org.bf2.arch.bot;
 
-public class CreateCommentTest {
+import java.io.IOException;
+
+import io.quarkiverse.githubapp.testing.GitHubAppTest;
+import io.quarkiverse.githubapp.testing.GitHubAppTesting;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GHEvent;
+import org.mockito.Mockito;
+
+@QuarkusTest
+@GitHubAppTest
+class CreateCommentTest {
+    @Test
+    void testIssueOpened() throws IOException {
+        GitHubAppTesting.when()
+                .payloadFromClasspath("/1-user-comment-issue.json")
+                .event(GHEvent.ISSUE_COMMENT)
+                .then().github(mocks -> {
+//                    Mockito.verify(mocks.pullRequest(949172137))
+//                            .getNumber();
+                });
+        GitHubAppTesting.when()
+                .payloadFromClasspath("/2-bot-open-pr.json")
+                .event(GHEvent.PULL_REQUEST)
+                .then().github(mocks -> {
+                    var n = mocks.issue(51).getNumber();
+                    var body = mocks.issueComment(1139495387).getBody();
+                    var y = mocks.pullRequest(949172137).getNumber();
+                    var x = mocks.pullRequest(51).getNumber();
+                    Mockito.verify(mocks.pullRequest(949172137))
+                            .setTitle("ADR-113: Create draft\nFixes #50");
+                });
+    }
 }
